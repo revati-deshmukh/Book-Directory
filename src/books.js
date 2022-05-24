42218db12b1d5f58f3ba2b561403712f5ac0430c
revati-deshmukh <deshmukh.revat@gmail.com>
2022-05-24 22:17:47 +0800

    Added postimg urls to show Book covers

--- src/books.js
+++ src/books.js
@@ -4,9 +4,9 @@ module.exports = [
         "isbn": "0143424629",
         "pageCount": 279,
         "publishedDate": {
-            "$date": "2016-15-10T00:00:00.000-0700"
+            "$date": "October 15th, 2016"
         },
-        "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg",
+        "thumbnailUrl": "https://i.postimg.cc/3d72x3Gm/girl-of-my-dreams.jpg",
         "shortDescription": "We are in the car She s looking at me. I can see the love in her eyes for me. Then a huge crash. She s flung out of the window. I m thrown out too. A pool of blood. Her eyes are still on me . . . but now it s a death stare. I am Daman and I wake up to this nightmare. Every. Single. Day.",
         "longDescription": "We are in the car She s looking at me. I can see the love in her eyes for me. Then a huge crash. She s flung out of the window. I m thrown out too. A pool of blood. Her eyes are still on me . . . but now it s a death stare. I am Daman and I wake up to this nightmare. Every. Single. Day. Waking up from a long coma Daman learns that he was in a massive car crash with a girl who vanished soon after the accident leaving him for dead. Strangely all he remembers is a hazy face her hypnotic eyes and her name Shreyasi. To come to terms with his memory lapse he starts piecing together stories about himself and Shreyasi from his dreams which he then turns into a hugelypopular blog. When he s offered a lucrative publishing deal to convert his blog pieces into a novel he signs up immediately. However he gives in to editorial pressure andagrees to corrupt the original edgy character of Shreyasi. Big mistake. From then on Daman is stalked and threatened by a terrifying beauty who claims to be Shreyasi and who will stop at nothing to make him pay for being a sell-out. Before Daman fights back he needs to know Is she really who she claims to be? What does she want from him now? What if he doesn t do what she wants him to?",
         "status": "PUBLISHED",
@@ -22,9 +22,9 @@ module.exports = [
         "isbn": "8173711461",
         "pageCount": 180,
         "publishedDate": {
-            "$date": "2011-11-01T00:00:00.000-0700"
+            "$date": "January 11th, 2011"
         },
-        "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg",
+        "thumbnailUrl": "https://i.postimg.cc/ppZz93Q7/wings-of-fire.jpg",
         "shortDescription": "Avul Pakir Jainulabdeen Abdul Kalam, the son of a little-educated boat-owner in Rameswaram, Tamil Nadu, had an unparalleled career as a defence scientist, culminating in the highest civilian award of India, the Bharat Ratna.",
         "longDescription": "Avul Pakir Jainulabdeen Abdul Kalam, the son of a little-educated boat-owner in Rameswaram, Tamil Nadu, had an unparalleled career as a defence scientist, culminating in the highest civilian award of India, the Bharat Ratna. As chief of the country's defence research and development programme, Kalam demonstrated the great potential for dynamism and innovation that existed in seemingly moribund research establishments. This is the story of Kalam's rise from obscurity and his personal and professional struggles, as well as the story of Agni, Prithvi, Akash, Trishul and Nag-missiles that have become household names in India and that have raised the nation to the level of a missile power of international reckoning. This is also the saga of independent India's struggle for technological self-sufficiency and defensive autonomy-a story as much about politics, domestic and international, as it is about science.",
         "status": "PUBLISHED",
@@ -40,9 +40,9 @@ module.exports = [
         "isbn": "1668002531",
         "pageCount": 487,
         "publishedDate": {
-            "$date": "2021-23-02T00:00:00.000-0700"
+            "$date": "February 23rd, 2021"
         },
-        "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg",
+        "thumbnailUrl": "https://i.postimg.cc/Bt2x3jjJ/spanish-love-deception.jpg",
         "shortDescription": "A TikTok sensation, this rom-com about a young woman who agrees to fake date a colleague and bring him to her sister's wedding has “everything you could want in a romance” (Helen Hoang, New York Times bestselling author). Catalina Martín desperately needs a date to her sister's wedding.",
         "longDescription": "A TikTok sensation, this rom-com about a young woman who agrees to fake date a colleague and bring him to her sister's wedding has “everything you could want in a romance” (Helen Hoang, New York Times bestselling author). Catalina Martín desperately needs a date to her sister's wedding.",
         "status": "PUBLISHED",
@@ -58,9 +58,9 @@ module.exports = [
         "isbn": "0593318315",
         "pageCount": 160,
         "publishedDate": {
-            "$date": "2021-27-04T00:00:00.000-0700"
+            "$date": "April 27th, 2021"
         },
-        "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg",
+        "thumbnailUrl": "https://i.postimg.cc/xX1HLgtd/whereabouts.jpg",
         "shortDescription": "A marvelous new novel from the Pulitzer Prize winning author of The Lowland and Interpreter of Maladies--her first in nearly a decade.",
         "longDescription": "Exuberance and dread, attachment and estrangement: in this novel, Jhumpa Lahiri stretches her themes to the limit. The woman at the center wavers between stasis and movement, between the need to belong and the refusal to form lasting ties. The city she calls home, an engaging backdrop to her days, acts as a confidant: the sidewalks around her house, parks, bridges, piazzas, streets, stores, coffee bars. We follow her to the pool she frequents and to the train station that sometimes leads her to her mother, mired in a desperate solitude after her father's untimely death. In addition to colleagues at work, where she never quite feels at ease, she has girl friends, guy friends, and 'him, ' a shadow who both consoles and unsettles her. But in the arc of a year, as one season gives way to the next, transformation awaits. One day at the sea, both overwhelmed and replenished by the sun's vital heat, her perspective will change.",
         "status": "PUBLISHED",
