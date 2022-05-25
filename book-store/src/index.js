a6f290294b2c3c0293e15edfa63fd40db029bacf
revati-deshmukh <deshmukh.revat@gmail.com>
2022-05-25 19:36:44 +0800

    Added reducer

--- book-store/src/index.js
+++ book-store/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = configureStore();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
