f864fa29d82cb7e83e43054b4bd1cc5c316a2b40
revati-deshmukh <deshmukh.revat@gmail.com>
2022-05-27 17:09:00 +0800

    Added App name

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -9,16 +9,36 @@ import {
   HomePage,
   AddBookPage
 } from './containers';
+import Container from '@mui/material/Container';
+import {
+  makeStyles
+} from '@material-ui/core';
+import { Typography } from '@mui/material';
+
+const useStyles = makeStyles((theme) => ({
+  h1: {
+    color: '#428284',
+    marginLeft: '0.5em !important'
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/:id" element={<EditBookPage />} />
-        <Route path="/addbook" element={<AddBookPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Container fixed>
+        <Typography variant="h3" className={classes.h1}>
+          Book Directory
+        </Typography>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/:id" element={<EditBookPage />} />
+            <Route path="/addbook" element={<AddBookPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </div>
   );
 }
 
