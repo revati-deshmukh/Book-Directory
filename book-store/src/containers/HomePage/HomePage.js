6c155c4b11895f39bd30c78c1adde4ca720acd29
revati-deshmukh <deshmukh.revat@gmail.com>
2022-05-27 17:09:35 +0800

    Added pagination to book list

--- book-store/src/containers/HomePage/HomePage.js
+++ book-store/src/containers/HomePage/HomePage.js
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchBooks } from './HomePage.duck';
 import { Link } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
+import TablePagination from '@mui/material/TablePagination';
 import Container from '@mui/material/Container';
 import ReactReadMoreReadLess from "react-read-more-read-less";
 import {
-    makeStyles
+    makeStyles,
+    Button
 } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
@@ -32,11 +35,32 @@ const useStyles = makeStyles((theme) => ({
     },
     link: {
         marginTop: 7
+    },
+    addBook: {
+        backgroundColor: '#3b989b',
+        float: 'right',
+        color: '#fff',
+        fontSize: 15
     }
+}));
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        fontSize: 18,
+        backgroundColor: '#74c6c9',
+        color: '#fff',
+        fontWeight: 500
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
 }));
 
 function HomePage() {
     const dispatch = useDispatch();
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
     const {
         fetchBooksInProgress,
         fetchBooksError,
@@ -49,59 +73,92 @@ function HomePage() {
 
     const classes = useStyles();
 
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 5));
+        setPage(0);
+    };
+
+    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - books.length) : 0;
+
     return (
         <div>
             {fetchBooksInProgress ? <p>Loading...</p> : null}
             {fetchBooksError ? <p>Error fetching books</p> : null}
             <Container fixed>
+                <Link to={`/addbook`}>
+                    <Button variant="contained" size="medium"
+                        className={classes.addBook}
+                        style={{ marginBottom: "10px" }}
+                    >Add Book
+                    </Button>
+                </Link>
                 <TableContainer>
                     <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
                         <TableHead>
                             <TableRow>
-                                <TableCell align="left">Book Cover</TableCell>
-                                <TableCell align="left">Title</TableCell>
-                                <TableCell align="left">Authors</TableCell>
-                                <TableCell align="left">Published</TableCell>
-                                <TableCell align="left">Categories</TableCell>
-                                <TableCell align="left">Description</TableCell>
+                                <StyledTableCell align="left">Book Cover</StyledTableCell>
+                                <StyledTableCell align="left">Title</StyledTableCell>
+                                <StyledTableCell align="left">Authors</StyledTableCell>
+                                <StyledTableCell align="left">Published</StyledTableCell>
+                                <StyledTableCell align="left">Categories</StyledTableCell>
+                                <StyledTableCell align="left">Description</StyledTableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {books.map((book, index) => {
-                                return (
-                                    <TableRow key={index}>
-                                        <TableCell className={classes.image} style={{ width: 100 }}>
-                                            <img className={classes.img} alt={book.title} src={book.thumbnailUrl}></img>
-                                        </TableCell>
-                                        <TableCell style={{ width: 200 }}>
-                                            <Link to={`/${book.isbn}`}>{book.title}</Link>
-                                        </TableCell>
-                                        <TableCell style={{ width: 100 }}>
-                                            {book.authors.join(", ")}
-                                        </TableCell>
-                                        <TableCell style={{ width: 100 }}>
-                                            {book.publishedDate.$date}
-                                        </TableCell>
-                                        <TableCell style={{ width: 160 }}>
-                                            {book.categories.join(", ")}
-                                        </TableCell>
-                                        <TableCell style={{ width: 200 }}>
-                                            <ReactReadMoreReadLess
-                                                charLimit={100}
-                                                readMoreText={"Read more ▼"}
-                                                readLessText={"Read less ▲"}
-                                            >
-                                                {book.shortDescription}
-                                            </ReactReadMoreReadLess>
-                                        </TableCell>
-                                    </TableRow>
-                                );
-                            })}
+                            {books.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((book, index) => {
+                                    return (
+                                        <TableRow key={index}>
+                                            <TableCell className={classes.image} style={{ width: 100 }}>
+                                                <img className={classes.img} alt={book.title} src={book.thumbnailUrl}></img>
+                                            </TableCell>
+                                            <TableCell style={{ width: 200 }}>
+                                                <Link to={`/${book.isbn}`}>{book.title}</Link>
+                                            </TableCell>
+                                            <TableCell style={{ width: 100 }}>
+                                                {book.authors.join(", ")}
+                                            </TableCell>
+                                            <TableCell style={{ width: 150 }}>
+                                                {book.publishedDate}
+                                            </TableCell>
+                                            <TableCell style={{ width: 160 }}>
+                                                {book.categories.join(", ")}
+                                            </TableCell>
+                                            <TableCell style={{ width: 200 }}>
+                                                <ReactReadMoreReadLess
+                                                    charLimit={100}
+                                                    readMoreText={"Read more ▼"}
+                                                    readLessText={"Read less ▲"}
+                                                >
+                                                    {book.shortDescription}
+                                                </ReactReadMoreReadLess>
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            {emptyRows > 0 && (
+                                <TableRow>
+                                    <TableCell colSpan={6} />
+                                </TableRow>
+                            )}
                         </TableBody>
                     </Table>
                 </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={books.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
             </Container>
-        </div>
+        </div >
     );
 }
 
