56a84762020c58e5b76cdfe1818a629c5b0e8de0
revati-deshmukh <deshmukh.revat@gmail.com>
2022-05-27 17:08:40 +0800

    Added more books to book list

--- src/books.js
+++ src/books.js
@@ -3,9 +3,7 @@ module.exports = [
         "title": "Girl of My Dreams",
         "isbn": "0143424629",
         "pageCount": 279,
-        "publishedDate": {
-            "$date": "October 15th, 2016"
-        },
+        "publishedDate": "October 15th, 2016",
         "thumbnailUrl": "https://i.postimg.cc/3d72x3Gm/girl-of-my-dreams.jpg",
         "shortDescription": "We are in the car She s looking at me. I can see the love in her eyes for me. Then a huge crash. She s flung out of the window. I m thrown out too. A pool of blood. Her eyes are still on me . . . but now it s a death stare. I am Daman and I wake up to this nightmare. Every. Single. Day.",
         "longDescription": "We are in the car She s looking at me. I can see the love in her eyes for me. Then a huge crash. She s flung out of the window. I m thrown out too. A pool of blood. Her eyes are still on me . . . but now it s a death stare. I am Daman and I wake up to this nightmare. Every. Single. Day. Waking up from a long coma Daman learns that he was in a massive car crash with a girl who vanished soon after the accident leaving him for dead. Strangely all he remembers is a hazy face her hypnotic eyes and her name Shreyasi. To come to terms with his memory lapse he starts piecing together stories about himself and Shreyasi from his dreams which he then turns into a hugelypopular blog. When he s offered a lucrative publishing deal to convert his blog pieces into a novel he signs up immediately. However he gives in to editorial pressure andagrees to corrupt the original edgy character of Shreyasi. Big mistake. From then on Daman is stalked and threatened by a terrifying beauty who claims to be Shreyasi and who will stop at nothing to make him pay for being a sell-out. Before Daman fights back he needs to know Is she really who she claims to be? What does she want from him now? What if he doesn t do what she wants him to?",
@@ -21,9 +19,7 @@ module.exports = [
         "title": "Wings of Fire: An Autobiography",
         "isbn": "8173711461",
         "pageCount": 180,
-        "publishedDate": {
-            "$date": "January 11th, 2011"
-        },
+        "publishedDate": "January 11th, 2011",
         "thumbnailUrl": "https://i.postimg.cc/ppZz93Q7/wings-of-fire.jpg",
         "shortDescription": "Avul Pakir Jainulabdeen Abdul Kalam, the son of a little-educated boat-owner in Rameswaram, Tamil Nadu, had an unparalleled career as a defence scientist, culminating in the highest civilian award of India, the Bharat Ratna.",
         "longDescription": "Avul Pakir Jainulabdeen Abdul Kalam, the son of a little-educated boat-owner in Rameswaram, Tamil Nadu, had an unparalleled career as a defence scientist, culminating in the highest civilian award of India, the Bharat Ratna. As chief of the country's defence research and development programme, Kalam demonstrated the great potential for dynamism and innovation that existed in seemingly moribund research establishments. This is the story of Kalam's rise from obscurity and his personal and professional struggles, as well as the story of Agni, Prithvi, Akash, Trishul and Nag-missiles that have become household names in India and that have raised the nation to the level of a missile power of international reckoning. This is also the saga of independent India's struggle for technological self-sufficiency and defensive autonomy-a story as much about politics, domestic and international, as it is about science.",
@@ -39,9 +35,7 @@ module.exports = [
         "title": "The Spanish Love Deception",
         "isbn": "1668002531",
         "pageCount": 487,
-        "publishedDate": {
-            "$date": "February 23rd, 2021"
-        },
+        "publishedDate": "February 23rd, 2021",
         "thumbnailUrl": "https://i.postimg.cc/Bt2x3jjJ/spanish-love-deception.jpg",
         "shortDescription": "A TikTok sensation, this rom-com about a young woman who agrees to fake date a colleague and bring him to her sister's wedding has “everything you could want in a romance” (Helen Hoang, New York Times bestselling author). Catalina Martín desperately needs a date to her sister's wedding.",
         "longDescription": "A TikTok sensation, this rom-com about a young woman who agrees to fake date a colleague and bring him to her sister's wedding has “everything you could want in a romance” (Helen Hoang, New York Times bestselling author). Catalina Martín desperately needs a date to her sister's wedding.",
@@ -57,9 +51,7 @@ module.exports = [
         "title": "Wherabouts",
         "isbn": "0593318315",
         "pageCount": 160,
-        "publishedDate": {
-            "$date": "April 27th, 2021"
-        },
+        "publishedDate": "April 27th, 2021",
         "thumbnailUrl": "https://i.postimg.cc/xX1HLgtd/whereabouts.jpg",
         "shortDescription": "A marvelous new novel from the Pulitzer Prize winning author of The Lowland and Interpreter of Maladies--her first in nearly a decade.",
         "longDescription": "Exuberance and dread, attachment and estrangement: in this novel, Jhumpa Lahiri stretches her themes to the limit. The woman at the center wavers between stasis and movement, between the need to belong and the refusal to form lasting ties. The city she calls home, an engaging backdrop to her days, acts as a confidant: the sidewalks around her house, parks, bridges, piazzas, streets, stores, coffee bars. We follow her to the pool she frequents and to the train station that sometimes leads her to her mother, mired in a desperate solitude after her father's untimely death. In addition to colleagues at work, where she never quite feels at ease, she has girl friends, guy friends, and 'him, ' a shadow who both consoles and unsettles her. But in the arc of a year, as one season gives way to the next, transformation awaits. One day at the sea, both overwhelmed and replenished by the sun's vital heat, her perspective will change.",
@@ -71,4 +63,102 @@ module.exports = [
             "Fiction"
         ]
     },
+    {
+        "title": "Breath: The New Science of a Lost Art",
+        "isbn": "0735213615",
+        "pageCount": 214,
+        "publishedDate": "May 26th 2020",
+        "thumbnailUrl": "https://i.postimg.cc/K3t1VZTL/breath.jpg",
+        "shortDescription": "No matter what you eat, how much you exercise, how skinny or young or wise you are, none of it matters if you're not breathing properly.",
+        "longDescription": "There is nothing more essential to our health and well-being than breathing: take air in, let it out, repeat twenty-five thousand times a day. Yet, as a species, humans have lost the ability to breathe correctly, with grave consequences. Journalist James Nestor travels the world to figure out what went wrong and how to fix it. The answers aren't found in pulmonology labs, as we might expect, but in the muddy digs of ancient burial sites, secret Soviet facilities, New Jersey choir schools, and the smoggy streets of Sao Paulo. Nestor tracks down men and women exploring the hidden science behind ancient breathing practices like Pranayama, Sudarshan Kriya, and Tummo and teams up with pulmonary tinkerers to scientifically test long-held beliefs about how we breathe.",
+        "status": "PUBLISHED",
+        "authors": [
+            "James Nestor"
+        ],
+        "categories": [
+            "Notification"
+        ]
+    },
+    {
+        "title": "Mrutyunjay",
+        "isbn": "8174210105",
+        "pageCount": 628,
+        "publishedDate": '1 January, 1967',
+        "thumbnailUrl": 'https://i.postimg.cc/zLXKf9YZ/mrutyunjay.jpg',
+        "shortDescription": "MRUTYUNJAY is a tragic novel based on the life of great warrior Karna.",
+        "longDescription": "MRUTYUNJAY is a tragic novel based on the life of great warrior Karna.",
+        "status": "PUBLISHED",
+        "authors": [
+            "Shivanji Sawant"
+        ],
+        "categories": [
+            "History"
+        ]
+    },
+    {
+        "title": "The Miracle of Mindfulness: An Introduction to the Practice of Meditation",
+        "isbn": "0807012394",
+        "pageCount": 140,
+        "publishedDate": "May 1st, 1999",
+        "thumbnailUrl": "https://i.postimg.cc/QVV3HxRj/mindfulness.jpg",
+        "shortDescription": "In this beautiful and lucid guide, Zen master Thich Nhat Hanh offers gentle anecdotes and practical exercise as a means of learning the skills of mindfulness--being awake and fully aware",
+        "longDescription": "In this beautiful and lucid guide, Zen master Thich Nhat Hanh offers gentle anecdotes and practical exercise as a means of learning the skills of mindfulness--being awake and fully aware. From washing the dishes to answering the phone to peeling an orange, he reminds us that each moment holds within it an opportunity to work toward greater self-understanding and peacefulness.",
+        "status": "PUBLISHED",
+        "authors": [
+            "Thich Nhat Hanh"
+        ],
+        "categories": [
+            "Notification"
+        ]
+    },
+    {
+        "title": "बटाट्याची चाळ",
+        "isbn": "817486833X",
+        "pageCount": 172,
+        "publishedDate": "1 January, 1958",
+        "thumbnailUrl": "https://i.postimg.cc/zHLZM0ny/batatyachichal.jpg",
+        "shortDescription": "Batatychi Chal, a story written by Purushottam Laxman Deshpande, is set in India during the 1940s.",
+        "longDescription": "The story focuses on the inhabitants of a tenement called Batatyachi Chal in Girgaon, which is a locality in the city of Mumbai. The families living there belong to the lower middle class, and the residents include music teachers, school teachers, clerks, and the likes. The humorous and poignant narrative laments the decline of this class of people. A cultural movement, a musical show, songs for women, and some pages from the diaries of the residents are some interesting elements from the book, and Dwarakanath Gupte, Baba Barve, Kochrekar, Kashinath Nadkarni, Janoba Rege, Sammel Kaka, Kushabhau Akshikar, and Raghunana Soman are some of the main characters from Batatychi Chal.",
+        "status": "PUBLISHED",
+        "authors": [
+            "P.L.Deshpande"
+        ],
+        "categories": [
+            "Humor",
+            "Fiction"
+        ]
+    },
+    {
+        "title": "Only When It's Us",
+        "isbn": "B085B9LFG2",
+        "pageCount": 382,
+        "publishedDate": "April 1st, 2020",
+        "thumbnailUrl": "https://i.postimg.cc/TL4BrgZf/onlywhenitsus.jpg",
+        "shortDescription": "Prepare for an emotional rollercoaster brimming with laughter, tears, and slow-burn sexiness in this new adult romance that tackles the vulnerability of love with humor and heart.",
+        "longDescription": "Prepare for an emotional rollercoaster brimming with laughter, tears, and slow-burn sexiness in this new adult romance that tackles the vulnerability of love with humor and heart.",
+        "status": "PUBLISHED",
+        "authors": [
+            "Chloe Liese"
+        ],
+        "categories": [
+            "Romance"
+        ]
+    },
+    {
+        "title": "The Power of Your Subconscious Mind",
+        "isbn": "0553583182",
+        "pageCount": 304,
+        "publishedDate": "January 2nd, 2001",
+        "thumbnailUrl": "https://i.postimg.cc/CB1WBmrc/subconsciousmind.jpg",
+        "shortDescription": "The Power of Your Subconscious Mind has been a bestseller since its first publication in 1963, selling many millions of copies since its original publication.",
+        "longDescription": "It is one of the most brilliant and beloved spiritual self-help works of all time which can help you heal yourself, banish your fears, sleep better, enjoy better relationships and just feel happier. The techniques are simple and results come quickly. You can improve your relationships, your finances, your physical well-being. Dr. Joseph Murphy explains that life events are actually the result of the workings of your conscious and subconscious minds. He suggests practical techniques through which one can change one's destiny, principally by focusing and redirecting this miraculous energy. Years of research studying the world's major religions convinced him that some Great Power lay behind all spiritual life and that this power is within each of us.",
+        "status": "PUBLISHED",
+        "authors": [
+            "Dr. Joseph Murphy",
+            "Ian McMahan"
+        ],
+        "categories": [
+            "Self Help"
+        ]
+    }
 ];
